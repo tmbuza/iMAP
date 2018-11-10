4381feeb23a6063dba7eed341ae665895d5ef166
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-10 11:25:06 -0600

    Update 01_processed_seqs.bash

--- code/annotation/01_processed_seqs.bash
+++ code/annotation/01_processed_seqs.bash
@@ -11,8 +11,8 @@ count.groups(count=./data/mothur/qced.trim.contigs.good.unique.good.filter.uniqu
 count.groups(count=./data/mothur/qced.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.count_table);
 count.groups(count=./data/mothur/phylotype/qced.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.pick.count_table)"
 
-paste -d'\t' ./data/mothur/phylotype/00_processed_seqs/qced.contigs.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.good.count.summary  ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.unique.good.filter.unique.precluster.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.pick.count.summary >./data/mothur/phylotype/00_processed_seqs/seqdepth2.tsv
+paste -d'\t' ./data/mothur/phylotype/00_processed_seqs/qced.contigs.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.good.count.summary  ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.unique.good.filter.unique.precluster.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.pick.count.summary >./data/mothur/phylotype/00_processed_seqs/seqdepth.tsv
 
 echo -e "SampleID\tOriginal\tScreened\tAligned\tPDenoised\tNonChimeric\tBacteriaOnly\tNoMock" >results/tables/processedSeqs.tsv
-cut -f1,2,4,6,8,10,12,14 ./data/mothur/phylotype/00_processed_seqs/seqdepth2.tsv >>results/tables/processedSeqs.tsv
+cut -f1,2,4,6,8,10,12,14 ./data/mothur/phylotype/00_processed_seqs/seqdepth.tsv >>results/tables/processedSeqs.tsv
 head results/tables/processedSeqs.tsv
