011d0a6bf332512a1f366bf3d2ccc04e8b531954
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-12 12:10:13 -0600

    Update iMAP_seqclassification_driver.bash

--- code/seqclassification/iMAP_seqclassification_driver.bash
+++ code/seqclassification/iMAP_seqclassification_driver.bash
@@ -1,2 +1,2 @@
 # bash code/seqclassification/00_download_taxonoy_references.bash
-./code/mothur/mothur code/seqclassification/01_classify_seqs.batch
+mothur code/seqclassification/01_classify_seqs.batch
