3e33fee41c41553c571ae5c218cb32478c11d185
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-12 10:40:03 -0600

    Update 01_multiqc_fastqc_summary.bash

--- code/summarizeFastQC/01_multiqc_fastqc_summary.bash
+++ code/summarizeFastQC/01_multiqc_fastqc_summary.bash
@@ -5,12 +5,12 @@
 # Check what version of python is installed on your computer by typping 'python --version'
 # For more details see the installation manual http://multiqc.info/docs/#manual-installation
 
-#pip install --upgrade pip
-#pip install --user multiqc
+#sudo pip install --upgrade pip
+#sudo pip install multiqc
 
 git clone https://github.com/ewels/MultiQC.git
 mv MultiQC ./code
-python ./code/MultiQC/setup.py install
+sudo python3 ./code/MultiQC/setup.py install
 
 mkdir results/multiqc
 mkdir results/multiqc/qc0
@@ -22,12 +22,12 @@ mkdir results/multiqc/qctrim25/R2
 mkdir results/multiqc/qced
 mkdir results/multiqc/qced/R1
 mkdir results/multiqc/qced/R2
-./code/MultiQC/scripts/multiqc  ./results/fastqc/qc0 -o ./results/multiqc/qc0
-./code/MultiQC/scripts/multiqc  ./results/fastqc/qc0 --ignore *_R2* -o ./results/multiqc/qc0/R1
-./code/MultiQC/scripts/multiqc  ./results/fastqc/qc0 --ignore *_R1* -o ./results/multiqc/qc0/R2
-./code/MultiQC/scripts/multiqc  ./results/fastqc/qctrim25 -o ./results/multiqc/qctrim25
-./code/MultiQC/scripts/multiqc  ./results/fastqc/qctrim25 --ignore *_R2* -o ./results/multiqc/qctrim25/R1
-./code/MultiQC/scripts/multiqc  ./results/fastqc/qctrim25 --ignore *_R1* -o ./results/multiqc/qctrim25/R2
-./code/MultiQC/scripts/multiqc ./results/fastqc/qced  -o ./results/multiqc/qced
-./code/MultiQC/scripts/multiqc ./results/fastqc/qced  --ignore *_R2* -o ./results/multiqc/qced/R1
-./code/MultiQC/scripts/multiqc ./results/fastqc/qced  --ignore *_R1* -o ./results/multiqc/qced/R2
+multiqc  ./results/fastqc/qc0 -o ./results/multiqc/qc0
+multiqc  ./results/fastqc/qc0 --ignore *_R2* -o ./results/multiqc/qc0/R1
+multiqc  ./results/fastqc/qc0 --ignore *_R1* -o ./results/multiqc/qc0/R2
+multiqc  ./results/fastqc/qctrim25 -o ./results/multiqc/qctrim25
+multiqc  ./results/fastqc/qctrim25 --ignore *_R2* -o ./results/multiqc/qctrim25/R1
+multiqc  ./results/fastqc/qctrim25 --ignore *_R1* -o ./results/multiqc/qctrim25/R2
+multiqc ./results/fastqc/qced  -o ./results/multiqc/qced
+multiqc ./results/fastqc/qced  --ignore *_R2* -o ./results/multiqc/qced/R1
+multiqc ./results/fastqc/qced  --ignore *_R1* -o ./results/multiqc/qced/R2
