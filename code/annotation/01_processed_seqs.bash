95878a5211e0da8af4801937e981aa8364525fe9
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-12 09:03:06 -0600

    Update 01_processed_seqs.bash

--- code/annotation/01_processed_seqs.bash
+++ code/annotation/01_processed_seqs.bash
@@ -13,7 +13,7 @@ count.groups(count=./data/mothur/phylotype/qced.trim.contigs.good.unique.good.fi
 
 paste -d'\t' ./data/mothur/phylotype/00_processed_seqs/qced.contigs.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.good.count.summary  ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.unique.good.filter.unique.precluster.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.count.summary ./data/mothur/phylotype/00_processed_seqs/qced.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.pick.count.summary >./data/mothur/phylotype/00_processed_seqs/seqdepth.tsv
 
-echo -e "SampleID\tOriginal\tScreened\tAligned\tPDenoised\tNonChimeric\tBacteriaOnly\tNoMock" >results/tables/processedSeqs.tsv
+echo -e "SampleID\tOriginal\tScreened\tAligned\tDenoised\tNonChimeric\tBacteriaOnly\tNoMock" >results/tables/processedSeqs.tsv
 cut -f1,2,4,6,8,10,12,14 ./data/mothur/phylotype/00_processed_seqs/seqdepth.tsv >>results/tables/processedSeqs.tsv
 
 head results/tables/processedSeqs.tsv
