6869090f8be2dc82896f8506643baabf87fc00a3
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-12 16:03:37 -0600

    Update 02_download_ref_alignments.bash

--- code/requirements/02_download_ref_alignments.bash
+++ code/requirements/02_download_ref_alignments.bash
@@ -35,7 +35,7 @@ mkdir data/references
 # ```{}
 wget --no-check-certificate https://www.mothur.org/w/images/7/71/Silva.seed_v132.tgz
 tar xvzf Silva.seed_v132.tgz silva.seed_v132.align silva.seed_v132.tax
-code/mothur/mothur "#get.lineage(fasta=silva.seed_v132.align, taxonomy=silva.seed_v132.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v132.pick.align, processors=8)"
+mothur "#get.lineage(fasta=silva.seed_v132.align, taxonomy=silva.seed_v132.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v132.pick.align, processors=8)"
 mv silva.seed_v132.pick.align data/references/silva.seed.align
 mv silva.seed_v132.pick.tax data/references/silva.seed.tax
 rm S*.tgz
@@ -43,6 +43,6 @@ rm silv*
 
 # # !For V4 datasets only
 # Generate a customized version of the SILVA reference database that targets the V4 region
-# code/mothur/mothur "#pcr.seqs(fasta=data/references/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
+# mothur "#pcr.seqs(fasta=data/references/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
 # mv data/references/silva.seed.pcr.align data/references/silva.seed.v4.align
 
