79eb0630fa17fef1b17ff07788b81cfde1cf2e62
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-12 18:46:11 -0600

    Update iMAP_requirements_linux_driver.bash

--- code/requirements/iMAP_requirements_linux_driver.bash
+++ code/requirements/iMAP_requirements_linux_driver.bash
@@ -1,5 +1,5 @@
 bash ./code/requirements/00_get_raw_data.bash
 bash ./code/requirements/01_install_linux_software.bash
 bash ./code/requirements/02_download_ref_alignments.bash
-bash ./code/requirements/03_download_taxonoy_classifiers.bash
+bash ./code/requirements/03_download_taxonomy_classifiers.bash
 
