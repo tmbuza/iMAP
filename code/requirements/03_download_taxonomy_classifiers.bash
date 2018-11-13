04b57bbb2f08189f6b7dc977f9c4e2859c9149c7
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-12 20:11:04 -0600

    Update 03_download_taxonomy_classifiers.bash

--- code/requirements/03_download_taxonomy_classifiers.bash
+++ code/requirements/03_download_taxonomy_classifiers.bash
@@ -43,16 +43,19 @@ mothur "#degap.seqs(fasta=./data/references/silva.seed.align)"
 # # ```
 
 # ##############################################################
-# # # !For V4 datasets only
-
-# #```{}
-#-------------------------------
-
-# Generate HMP_MOCK.fasta - an unaligned fasta sequence file
+# Generate HMP_MOCK.v4.fasta - an unaligned fasta sequence file that contains the V4 region of
+# the sequences in the mock community
 
 wget --no-check-certificate https://www.mothur.org/MiSeqDevelopmentData/HMP_MOCK.fasta
 mv HMP_MOCK.fasta data/references
 
+# # silva nr alignment
+# mothur "#align.seqs(fasta=data/references/HMP_MOCK.fasta, reference=data/references/silva.nr.align);degap.seqs()"
+# mv data/references/HMP_MOCK.ng.fasta data/references/HMP_MOCK.nr.v4.fasta
+
+# silva seed alignment
+/mothur "#align.seqs(fasta=data/references/HMP_MOCK.fasta, reference=data/references/silva.seed.align);degap.seqs()"
+mv data/references/HMP_MOCK.ng.fasta data/references/HMP_MOCK.seed.fasta
 # # #```
 
 # #```{}
