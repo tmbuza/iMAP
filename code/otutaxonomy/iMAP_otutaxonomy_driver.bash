dda51c03bf170f18c48fff18fe3440702f26dda5
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-13 00:14:41 -0600

    Update iMAP_otutaxonomy_driver.bash

--- code/otutaxonomy/iMAP_otutaxonomy_driver.bash
+++ code/otutaxonomy/iMAP_otutaxonomy_driver.bash
@@ -1,3 +1,3 @@
-./code/mothur/mothur code/otutaxonomy/01_get_shared_phylotype.batch
-./code/mothur/mothur code/otutaxonomy/02_get_shared_otus.batch
-./code/mothur/mothur code/otutaxonomy/03_get_shared_phylogeny_taxonomy.batch
+mothur code/otutaxonomy/01_get_shared_phylotype.batch
+mothur code/otutaxonomy/02_get_shared_otus.batch
+mothur code/otutaxonomy/03_get_shared_phylogeny_taxonomy.batch
