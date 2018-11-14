9c555db60f44bd895520eab5d9d02a7461575dd3
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-14 11:35:59 -0600

    Update 01_install_linux_software.bash

--- code/requirements/01_install_linux_software.bash
+++ code/requirements/01_install_linux_software.bash
@@ -10,6 +10,7 @@
 wget --no-check-certificate https://github.com/shenwei356/seqkit/releases/download/v0.8.0/seqkit_linux_amd64.tar.gz
 tar -zxvf seqkit_linux_amd64.tar.gz
 mv seqkit code/
+cp code/seqkit ~/bin
 rm seqkit_linux_amd64.tar.gz
 
 # Mac version
@@ -32,6 +33,7 @@ wget --no-check-certificate https://www.bioinformatics.babraham.ac.uk/projects/f
 unzip fastqc_v0.11.7.zip
 chmod 755 FastQC/fastqc
 mv -v FastQC code/
+cp code/FastQC/fastqc ~/bin/
 rm fastqc_v0.11.7.zip
 # ```
 
@@ -47,6 +49,7 @@ rm fastqc_v0.11.7.zip
 wget --no-check-certificate https://sourceforge.net/projects/bbmap/files/BBMap_37.90.tar.gz
 tar -xvzf BBMap_37.90.tar.gz
 mv -v bbmap code/
+cp code/bbmap/bbduk.sh ~/bin/
 rm BBMap_37.90.tar.gz
 
 
@@ -59,9 +62,11 @@ rm BBMap_37.90.tar.gz
 wget --no-check-certificate https://github.com/mothur/mothur/releases/download/v1.39.5/Mothur.linux_64.zip
 unzip Mothur.linux_64.zip
 mv mothur code/
+cp code/mothur/mothur ~/bin/
+cp code/mothur/uchime ~/bin/
+cp code/mothur/vsearch ~/bin/
 rm Mothur.linux_64.zip
 rm -rf __MACOSX
-
 # # Mac
 # wget --no-check-certificate https://github.com/mothur/mothur/releases/download/v1.39.5/Mothur.mac_64.OSX-10.12.zip
 # unzip Mothur.mac_64.OSX-10.12.zip
