2f478d0f1ede33a0145ce666c36abdc6833de149
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2018-11-24 22:15:32 -0600

    Update iMAP_seqprocessing_driver.bash

--- code/seqprocessing/iMAP_seqprocessing_driver.bash
+++ code/seqprocessing/iMAP_seqprocessing_driver.bash
@@ -1,6 +1,5 @@
 mkdir data
 mkdir data/references
-bash code/seqprocessing/00_add_pseudo_mock.bash
 mothur code/seqprocessing/01_assemble_paired_reads.batch
 mothur code/seqprocessing/02_align_for_16S_consensus.batch
 mothur code/seqprocessing/03_perform_alignment_QC.batch
