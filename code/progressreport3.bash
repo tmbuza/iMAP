e4f28625a27ec959b6e1f17f01c71526efbacfe9
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2019-02-09 19:37:24 -0600

    Update progressreport3.bash

--- code/progressreport3.bash
+++ code/progressreport3.bash
@@ -1,5 +1,3 @@
 !#/usr/bin/bash
 
 Rscript -e "rmarkdown::render('code/Rmd/report3.Rmd', clean=TRUE, quiet=FALSE, output_file='report3_sequence_processing.html', output_dir='reports')"
-# Rscript -e "rmarkdown::render('code/Rmd/report4.Rmd', clean=TRUE, quiet=FALSE, output_file='report4_read_analysis.html', output_dir='reports')"
-# Rscript -e "rmarkdown::render('code/Rmd/finalreport.Rmd', clean=TRUE, quiet=FALSE, output_file='iMAP_finalreport.html', output_dir='reports')"
