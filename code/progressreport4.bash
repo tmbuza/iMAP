48a01dc8ac5fd457fdd88b64858b5cf52f951efe
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2019-02-09 19:44:32 -0600

    Update progressreport4.bash

--- code/progressreport4.bash
+++ code/progressreport4.bash
@@ -1,3 +1,3 @@
-!#/usr/bin/bash
+#!/usr/bin/bash
 
 Rscript -e "rmarkdown::render('code/Rmd/report4.Rmd', clean=TRUE, quiet=FALSE, output_file='report4_preliminary_analysis.html', output_dir='reports')"
