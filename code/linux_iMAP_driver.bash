98f13f4e98bd844987cbe80ed9a8eb968e27fd39
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2019-02-09 18:05:01 -0600

    Update linux_iMAP_driver.bash

--- code/linux_iMAP_driver.bash
+++ code/linux_iMAP_driver.bash
@@ -1,12 +1,37 @@
-bash ./code/metadataprofiling/iMAP_metadata_profiling_driver.bash
-bash ./code/requirements/iMAP_requirements_linux_driver.bash
-bash ./code/requirements/iMAP_checkFiles_driver.bash
+# bash ./code/metadataprofiling/iMAP_metadata_profiling_driver.bash
+# bash ./code/requirements/iMAP_requirements_linux_driver.bash
+# bash ./code/requirements/iMAP_checkFiles_driver.bash
+# bash ./code/preprocessing/iMAP_preprocessing_driver.bash
+# bash ./code/summarizeFastQC/iMAP_multiqc_driver.bash
+# bash ./code/mockcommunity/iMAP_mockcommunity_driver.bash
+# bash ./code/seqprocessing/iMAP_seqprocessing_driver.bash
+# bash ./code/seqclassification/iMAP_seqclassification_driver.bash
+# bash ./code/seqerrorrate/iMAP_seqerrorrate_driver.bash
+# bash ./code/otutaxonomy/iMAP_otutaxonomy_driver.bash
+# bash ./code/annotation/iMAP_annotation_driver.bash
+# bash ./code/dataanalysis/iMAP_dataanalysis_demo_driver.bash
+
+
+
+## Metadata Profiling
+bash ./code/progressreport1.bash # Revised on 02/06/2019
+
+## Read Preprocessing
 bash ./code/preprocessing/iMAP_preprocessing_driver.bash
 bash ./code/summarizeFastQC/iMAP_multiqc_driver.bash
+bash ./code/progressreport2.bash # Revised on 02/06/2019
+
+## Sequence Processing
 bash ./code/mockcommunity/iMAP_mockcommunity_driver.bash
 bash ./code/seqprocessing/iMAP_seqprocessing_driver.bash
 bash ./code/seqclassification/iMAP_seqclassification_driver.bash
-bash ./code/seqerrorrate/iMAP_seqerrorrate_driver.bash
+bash ./code/seqerrorrate/iMAP_seqerrorrate_driver.bash # Optional
 bash ./code/otutaxonomy/iMAP_otutaxonomy_driver.bash
-bash ./code/annotation/iMAP_annotation_driver.bash
-bash ./code/dataanalysis/iMAP_dataanalysis_demo_driver.bash
+bash ./code/annotation/01_processed_seqs.bash # Summarizing processed sequences
+bash ./code/annotation/02_merge_summary_files.bash # Merge summary files to evaluate read length
+bash ./code/progressreport3.bash # Revised on 02/06/2019
+bash ./code/datatransformation.bash # Revised on 02/09/2019
+
+## Preliminary Analysis
+bash ./code/dataanalysis/iMAP_dataanalysis_demo_driver.bash # Optional mothur-based preliminary analysis
+bash ./code/progressreport4.bash # Revision in progress
