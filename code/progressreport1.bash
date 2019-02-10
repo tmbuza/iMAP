736eed6d1d89b7214de55cf63023337c853498f5
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2019-02-09 19:45:42 -0600

    Update progressreport1.bash

--- code/progressreport1.bash
+++ code/progressreport1.bash
@@ -1,3 +1,3 @@
-!#/usr/bin/bash
+#!/usr/bin/bash
 
 Rscript -e "rmarkdown::render('code/Rmd/report1.Rmd', clean=TRUE, quiet=FALSE, output_file='report1_metadataprofiling.html', output_dir='reports')"
