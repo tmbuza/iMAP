fb146155f8b1f1771cf59e43fc4b6bcc41aee588
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2019-02-09 19:45:17 -0600

    Update progressreport2.bash

--- code/progressreport2.bash
+++ code/progressreport2.bash
@@ -1,3 +1,3 @@
-!#/usr/bin/bash
+#!/usr/bin/bash
 
 Rscript -e "rmarkdown::render('code/Rmd/report2.Rmd', clean=TRUE, quiet=FALSE, output_file='report2_read_preprocessing.html', output_dir='reports')"
