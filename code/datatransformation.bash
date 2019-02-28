01841b562273b3b0386c69ff5fa5ca0a86f406ec
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2019-02-28 13:39:57 -0600

    Update datatransformation.bash

--- code/datatransformation.bash
+++ code/datatransformation.bash
@@ -1,4 +1,4 @@
-!#/usr/bin/bash
+#!/usr/bin/bash
 
 # Rscript -e "rmarkdown::render('code/Rmd/report3.Rmd', clean=TRUE, quiet=FALSE, output_file='report3_sequence_processing.html', output_dir='reports')"
 Rscript -e "rmarkdown::render('code/Rmd/phylotype_constaxonomy_transformed.Rmd', clean=TRUE, quiet=FALSE, output_dir='results/tables/phylotype')"
