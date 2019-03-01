e8cbf5647a3aee3598653b97f74e7cdb028c6c9e
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2019-03-01 08:30:58 -0600

    Delete iMAP_requirements_windows_driver.bash

--- code/requirements/iMAP_requirements_windows_driver.bash
+++
@@ -1,4 +0,0 @@
-bash ./code/requirements/00_get_raw_data.bash
-bash ./code/requirements/01_install_windows_software.bash
-bash ./code/requirements/02_download_ref_alignments.bash
-bash ./code/requirements/03_download_taxonomy_classifiers.bash
