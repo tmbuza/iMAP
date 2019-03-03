d982135452b9fae6b84d9fc515186a9505536317
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2019-03-03 17:19:40 -0600

    Update 01_install_linux_software.bash

--- code/requirements/01_install_linux_software.bash
+++ code/requirements/01_install_linux_software.bash
@@ -60,8 +60,9 @@ rm BBMap_37.90.tar.gz
 #!Important: Latest versions may not work well until it is stable. Should that hapen, just install a previous version.
 
 # #Linux
-##wget --no-check-certificate https://github.com/mothur/mothur/releases/download/v1.40.0.1/Mothur.linux_64.zip
-wget --no-check-certificate https://github.com/mothur/mothur/releases/download/v1.39.5/Mothur.linux_64.zip
+wget --no-check-certificate https://github.com/mothur/mothur/releases/download/v1.41.3/Mothur.linux_64.zip
+# wget --no-check-certificate https://github.com/mothur/mothur/releases/download/v1.40.0.1/Mothur.linux_64.zip
+# wget --no-check-certificate https://github.com/mothur/mothur/releases/download/v1.39.5/Mothur.linux_64.zip
 unzip Mothur.linux_64.zip
 mv mothur code/
 cp code/mothur/mothur ~/bin/
