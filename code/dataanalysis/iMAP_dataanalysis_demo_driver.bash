dc8f196b9d1beb715dccb8046bc69a527e34d406
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-03-17 00:59:10 -0500

    Update and rename iMAP_dataanalysis_demo_driver.bash to iMAP_dataanalysis_driver.bash

--- code/dataanalysis/iMAP_dataanalysis_demo_driver.bash
+++
@@ -1,3 +0,0 @@
-mothur code/dataanalysis/01_phylotype_demo_analysis.batch
-# mothur code/dataanalysis/02_otus_demo_analysis.batch
-mothur code/dataanalysis/03_phylogeny_demo_analysis.batch
---
+++ code/dataanalysis/iMAP_dataanalysis_driver.bash
@@ -0,0 +1,3 @@
+mothur code/dataanalysis/01_phylotype_analysis.batch
+# mothur code/dataanalysis/02_otus_analysis.batch
+mothur code/dataanalysis/03_phylogeny_analysis.batch
