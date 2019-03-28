e229079864284e16ba460cfd7f49db64abb3f035
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-03-28 11:41:57 -0500

    Update iMAP_seqprocessing_driver.bash

--- code/seqprocessing/iMAP_seqprocessing_driver.bash
+++ code/seqprocessing/iMAP_seqprocessing_driver.bash
@@ -2,6 +2,6 @@ mkdir data
 mkdir data/references
 mothur code/seqprocessing/01_assemble_paired_reads.batch
 mothur code/seqprocessing/02_align_for_16S_consensus.batch
-mothur code/seqprocessing/03_perform_alignment_QC.batch
+mothur code/seqprocessing/03_denoise_alignments.batch
 mothur code/seqprocessing/04_remove_chimeras.batch
 
