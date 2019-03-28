a76ad5e1cda41b3a4df115810a41db47e901d9db
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-03-27 21:27:55 -0500

    Update 00_checkFiles_driver.bash

--- code/00_checkFiles_driver.bash
+++ code/00_checkFiles_driver.bash
@@ -1,10 +1,4 @@
 ## Checking if the foler/file exist and save the output in report folder in a file named checked_files.txt
 
-# bash ./code/requirements/04_check_folders_and_files.bash > ./reports/checked_files.txt
-bash ./code/requirements/04_check_folders_and_files.bash
-
-## View the output
-#open ./reports/checked_files.txt
-
-## Save the output
+bash/requirements/iMAP_checkFiles_driver.basht
 
