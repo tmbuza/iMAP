833b043df03e281b8a8892b0616282b686bd2e89
tmbuza <ndelly@gmail.com>
2019-03-28 15:34:48 -0500

    Update 00_checkFiles_driver.bash

--- code/00_checkFiles_driver.bash
+++ code/00_checkFiles_driver.bash
@@ -1,4 +1,4 @@
 ## Checking if the foler/file exist and save the output in report folder in a file named checked_files.txt
 
-bash code/requirements/iMAP_checkFiles_driver.bash
+bash iMAP/code/requirements/iMAP_checkFiles_driver.bash
 
