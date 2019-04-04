e5d6632c41c75953026f664fbc74a741079de49d
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-04-04 08:49:09 -0500

    Update 04_OTUanalysis_driver.bash

--- code/04_OTUanalysis_driver.bash
+++ code/04_OTUanalysis_driver.bash
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 bash ./code/dataanalysis/iMAP_dataanalysis_driver.bash
+
+if [ "$?" != "0" ]; then
+    echo "[Error] Sorry, OTU anaysis did not finish, exiting...,!" 1>&2
+    exit 1
+fi
+
+
