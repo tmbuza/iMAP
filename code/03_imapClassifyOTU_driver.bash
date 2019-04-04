0e702f19df5e732faae08f29e61176ed2eb448f9
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-04-04 08:44:51 -0500

    Update 03_imapClassifyOTU_driver.bash

--- code/03_imapClassifyOTU_driver.bash
+++ code/03_imapClassifyOTU_driver.bash
@@ -3,17 +3,44 @@
 ## Mothur-based Sequence Processing and classification
 bash ./code/refdatabase/iMAP_download_driver.bash
 
+if [ "$?" != "0" ]; then
+    echo "[Error] Sorry, missing one or more required reference database, exiting...!" 1>&2
+    exit 1
+fi
+
 
 bash ./code/seqprocessing/iMAP_seqprocessing_driver.bash
+
+if [ "$?" != "0" ]; then
+    echo "[Error] Sorry, sequence processing did not finish, exiting...,!" 1>&2
+    exit 1
+fi
 
 
 bash ./code/seqclassification/iMAP_seqclassification_driver.bash
 
 
+if [ "$?" != "0" ]; then
+    echo "[Error] Sorry, sequence classification did not finish, exiting...,!" 1>&2
+    exit 1
+fi
+
+
 bash ./code/seqerrorrate/iMAP_seqerrorrate_driver.bash
+
+if [ "$?" != "0" ]; then
+    echo "[Error] Sorry, error estimation did not finish, exiting...,!" 1>&2
+    exit 1
+fi
+
 
 
 bash ./code/otutaxonomy/iMAP_otutaxonomy_driver.bash
+
+if [ "$?" != "0" ]; then
+    echo "[Error] Sorry, OTU taxonomy assignment failed, exiting...,!" 1>&2
+    exit 1
+fi
 
 
 # bash ./code/annotation/01_processed_seqs.bash # Summarizing processed sequences
