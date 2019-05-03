3c2a1de0da4cc1432fb978afce63ee7bc6c1ae0e
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-05-03 16:32:53 -0500

    Update demo_data.bash

--- demo_data.bash
+++ demo_data.bash
@@ -5,5 +5,5 @@
 cp iMAP/resources/metadata/samplemetadata.tsv iMAP/data/metadata/
 cp iMAP/resources/qced.files iMAP/data/metadata/
 cp iMAP/resources/manifest.txt iMAP/data/metadata/
-unzip -oj iMAP/resources/bbmap.zip -d iMAP/code/
+unzip -o iMAP/resources/bbmap.zip -d iMAP/code/
 unzip -oj iMAP/resources/raw.zip -d iMAP/data/raw/
