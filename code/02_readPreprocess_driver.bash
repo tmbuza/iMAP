d350f63e753cf33ce58dace56c09b8a536af56fd
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-05-04 18:11:19 -0500

    Update 02_readPreprocess_driver.bash

--- code/02_readPreprocess_driver.bash
+++ code/02_readPreprocess_driver.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## Install bbmap tools (didn't work in docker image)
-# bash ./code/requirements/iMAP_additional_software_driver.bash
+bash ./code/requirements/iMAP_additional_software_driver.bash
 
 ## Download reference databases
 # bash ./code/refdatabase/iMAP_download_driver.bash
