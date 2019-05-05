d41a8d5b243ba3963ca000243f68006ac36457a2
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-05-05 17:50:24 -0500

    Update 04_3_phylogeny_driver.bash

--- code/04_3_phylogeny_driver.bash
+++ code/04_3_phylogeny_driver.bash
@@ -9,6 +9,6 @@ mothur code/dataanalysis/03_phylogeny_analysis.batch
 
 
 if [ "$?" != "0" ]; then
-   echo "[Error] Sorry, phylogeny-based analysis did not finish, exiting...,!" 1>&2
+   echo "[Error] Sorry, phylogeny-based  preliminary analysis did not finish, exiting...,!" 1>&2
     exit 1
 fi
