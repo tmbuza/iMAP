daeedbb0e7e6ce2d5c1b3c160bc580bb431dd529
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-05-05 17:48:38 -0500

    Update 04_2_otucluster_driver.bash

--- code/04_2_otucluster_driver.bash
+++ code/04_2_otucluster_driver.bash
@@ -5,3 +5,10 @@ if [ "$?" != "0" ]; then
     exit 1
 fi
 
+mothur code/dataanalysis/03_phylogeny_analysis.batch
+
+
+if [ "$?" != "0" ]; then
+   echo "[Error] Sorry, phylogeny-based analysis did not finish, exiting...,!" 1>&2
+    exit 1
+fi
