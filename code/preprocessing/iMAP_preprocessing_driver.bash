0e88a5b387a7e7fa9adca75b711351048276b60c
tmbuza <ndelly@gmail.com>
2019-05-05 20:34:27 -0500

    Update iMAP_preprocessing_driver.bash

--- code/preprocessing/iMAP_preprocessing_driver.bash
+++ code/preprocessing/iMAP_preprocessing_driver.bash
@@ -4,8 +4,8 @@
 
 mkdir data results
 mkdir data reports
-mkdir data/raw 
-mkdir results/stats 
+mkdir data/raw
+mkdir results/stats
 mkdir results/fastqc
 mkdir results/fastqc/qc0
 mkdir results/fastqc/qctrim25
@@ -14,12 +14,12 @@ mkdir results/multiqc
 mkdir results/multiqc/qc0
 mkdir results/multiqc/qctrim25
 mkdir results/multiqc/qced
-mkdir results/tables 
+mkdir results/tables
 mkdir results/figures
 
 ## Start pre-processing
-#bash code/preprocessing/02_get_read_statistics.bash
-#bash code/preprocessing/03_get_basecall_scores.bash
+bash code/preprocessing/02_get_read_statistics.bash
+bash code/preprocessing/03_get_basecall_scores.bash
 bash code/preprocessing/04_get_highscore_reads.bash
 bash code/preprocessing/05_get_qced_reads.bash
 bash code/preprocessing/06_final_seqkit.bash
