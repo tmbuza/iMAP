2540b22594c36cabee0d1d714fe7bf5d32daf6eb
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-05-06 00:51:52 -0500

    Update qiime2_gg_classifier_fulllength.bash

--- code/qiime2/qiime2_gg_classifier_fulllength.bash
+++ code/qiime2/qiime2_gg_classifier_fulllength.bash
@@ -1,4 +1,4 @@
-wget -O ./data/references/gg-13-8-99-nb-classifier.qza https://data.qiime2.org/2019.4/common/gg-13-8-99-nb-classifier.qza
+wget -O ./iMAP/data/references/gg-13-8-99-nb-classifier.qza https://data.qiime2.org/2019.4/common/gg-13-8-99-nb-classifier.qza
 
 if [ "$?" != "0" ]; then
     echo "[Error] Sorry, download of qiime2 classifier failed, exiting...,!" 1>&2
