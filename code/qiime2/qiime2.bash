f8905285402d45ea03a004cac4f8be145755b16a
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-05-06 10:15:15 -0500

    Update qiime2.bash

--- code/qiime2/qiime2.bash
+++ code/qiime2/qiime2.bash
@@ -91,14 +91,10 @@ qiime phylogeny midpoint-root \
 ## TAXONOMY ANALYSIS
 ### Greengenes classifiation
 time qiime feature-classifier classify-sklearn \
-  --i-classifier $PWD/data/references/gg-13-8-99-nb-classifier.qza \
+  #   --i-classifier $PWD/data/references/gg-13-8-99-nb-classifier.qza \
+  --i-classifier $PWD/data/references/gg-13-8-99-515-806-nb-classifier.qza \
   --i-reads $PWD/data/qiime2/rep-seqs.qza \
   --o-classification $PWD/data/qiime2/greengenes-taxonomy.qza
-
-# time qiime feature-classifier classify-sklearn \
-#   --i-classifier $PWD/data/references/gg-13-8-99-515-806-nb-classifier.qza \
-#   --i-reads $PWD/data/qiime2/rep-seqs.qza \
-#   --o-classification $PWD/data/qiime2/greengenes-taxonomy.qza
 
 ### View greengenes Taxonomy classification
 qiime metadata tabulate \
