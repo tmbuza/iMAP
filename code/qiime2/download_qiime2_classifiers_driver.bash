a6c4e0ea32eee11e5b87125b00212812e7f8a03b
Teresia Mrema Buza <tmbuza@microbiome-bioinfo.com>
2019-05-06 00:29:17 -0500

    Update download_qiime2_classifiers_driver.bash

--- code/qiime2/download_qiime2_classifiers_driver.bash
+++ code/qiime2/download_qiime2_classifiers_driver.bash
@@ -1,1 +1,3 @@
 wget -O "./data/references/gg-13-8-99-515-806-nb-classifier.qza" "https://data.qiime2.org/2019.4/common/gg-13-8-99-515-806-nb-classifier.qza")
+
+# wget -O "./data/references/gg-13-8-99-nb-classifier.qza" "https://data.qiime2.org/2019.4/common/gg-13-8-99-nb-classifier.qza")
