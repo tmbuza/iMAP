33fd7cda759e938dce48876d52b088a465a78f2a
TMBiz <ndelly@gmail.com>
2019-10-29 22:01:21 -0500

    Update code/installSoftware/00_install_linux_software.bash

--- code/installSoftware/00_install_linux_software.bash
+++ code/installSoftware/00_install_linux_software.bash
@@ -4,7 +4,8 @@
 
 
 ## Create bin folder in home directory (if it doesn't exist)
-sudo mkdir $HOME/bin
+cd ~/
+sudo mkdir bin
 
 
 ## Install dependencies as necessary.
@@ -51,7 +52,7 @@ sudo apt-get clean
 sudo wget --no-check-certificate https://github.com/shenwei356/seqkit/releases/download/v0.8.0/seqkit_linux_amd64.tar.gz
 sudo tar -zxvf seqkit_linux_amd64.tar.gz
 sudo chmod 755 seqkit
-	sudo cp seqkit $HOME/bin/seqkit
+	sudo ln -s seqkit bin/seqkit
 	sudo  rm -f seqkit_linux_amd64.tar.gz
 
 # which seqkit # Must show the location of seqkit
@@ -63,7 +64,7 @@ sudo chmod 755 seqkit
 sudo wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip
 sudo unzip fastqc_*.zip
 sudo chmod 755 FastQC/fastqc
-    sudo  cp -r FastQC $HOME/bin/FastQC
+    sudo ln -s FastQC/fastqc bin/fastqc
     sudo  rm -f fastqc_*.zip 
 
 # which fastqc # Must show the location of fastqc
@@ -75,7 +76,7 @@ sudo chmod 755 FastQC/fastqc
 sudo wget --no-check-certificate https://sourceforge.net/projects/bbmap/files/BBMap_38.69.tar.gz
 sudo tar -xvzf BBMap_38.69.tar.gz
 sudo chmod 755 bbmap/bbduk.sh
-    sudo cp bbmap/bbduk.sh $HOME/bin/bbduk.sh
+    sudo ln -s bbmap/bbduk.sh bin/bbduk.sh
     sudo  rm BBMap_38.69.tar.gz
 
 # which bbduk.sh # Must show the location of bbduk.sh 
@@ -83,8 +84,7 @@ sudo chmod 755 bbmap/bbduk.sh
 
 ## Summarize Base Call Phred Scores
 sudo pip3 --disable-pip-version-check --no-cache-dir install multiqc #Probably installed in /usr/local/bin/multiqc
-sudo ln -s /usr/local/bin/multiqc $HOME/bin/multiqc
-sudo ln -s /usr/local/bin/multiqc ./multiqc
+sudo ln -s /usr/local/bin/multiqc bin/multiqc
 
 
 
@@ -97,9 +97,9 @@ sudo ln -s /usr/local/bin/multiqc ./multiqc
 # sudo chmod 755 mothur/vsearch
 # sudo chmod 755 mothur/uchime
 #     sudo  mv -v mothur $HOME/
-#     sudo cp mothur/mothur $HOME/bin/mothur
-#     sudo cp mothur/vsearch $HOME/bin/vsearch
-#     sudo cp mothur/uchime $HOME/bin/uchime
+#     sudo ln -s mothur/mothur bin/mothur
+#     sudo ln -s mothur/vsearch bin/vsearch
+#     sudo ln -s mothur/uchime bin/uchime
 # sudo  rm -rf Mothur.linux_64.zip 
 
 
@@ -108,7 +108,7 @@ sudo ln -s /usr/local/bin/multiqc ./multiqc
 ##### mothur error while loading shared libraries: libreadline.so.6: cannot open shared object file: No such file or directory
 ## Try this, it may or may not work.
 # cd /lib/x86_64-linux-gnu
-# sudo sudo cp libreadline.so.7.0 libreadline.so.6
+# sudo sudo ln -s libreadline.so.7.0 libreadline.so.6
 
 ## Hurray it worked!
 
@@ -119,9 +119,9 @@ sudo chmod 755 mothur/mothur
 sudo chmod 755 mothur/vsearch
 sudo chmod 755 mothur/uchime
 
-sudo cp ./mothur/mothur $HOME/bin/mothur
-sudo cp ./mothur/vsearch $HOME/bin/vsearch
-sudo cp ./mothur/uchime $HOME/bin/uchime
+sudo ln -s mothur/mothur bin/mothur
+sudo ln -s mothur/vsearch bin/vsearch
+sudo ln -s mothur/uchime bin/uchime
 
 
 
@@ -134,9 +134,9 @@ sudo  rm -rf Mothur.linux_noReadline.zip
 # sudo chmod 755 mothur/vsearch
 # sudo chmod 755 mothur/uchime
 #     sudo  mv -v mothur $HOME/
-#     sudo cp mothur/mothur $HOME/bin/mothur
-#     sudo cp mothur/vsearch $HOME/bin/vsearch
-#     sudo cp mothur/uchime $HOME/bin/uchime
+#     sudo ln -s mothur/mothur bin/mothur
+#     sudo ln -s mothur/vsearch bin/vsearch
+#     sudo ln -s mothur/uchime bin/uchime
 # 	sudo  rm -rf Mothur.Ubuntu_18.zip 
 
 
@@ -160,6 +160,6 @@ echo ""
 
 
 # Added to the $HOME/.bashrc file
-# echo "export PATH=$PATH:$HOME::$HOME/bin:$HOME/FastQC:$HOME/bbmap:$HOME/mothur" >> ~/.bashrc
+# echo "export PATH=$PATH:$HOME::bin:$HOME/FastQC:$HOME/bbmap:$HOME/mothur" >> ~/.bashrc
 # source  ~/.bashrc
 
