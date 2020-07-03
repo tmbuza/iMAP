6ad884411f44c9cadb24eccf32ac296fa76de6c5
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2020-07-03 11:14:10 -0500

    Update qiime2_gg_classifier_fragments.bash

--- code/qiime2/qiime2_gg_classifier_fragments.bash
+++ code/qiime2/qiime2_gg_classifier_fragments.bash
@@ -1,4 +1,4 @@
-wget -O ./iMAP/data/references/gg-13-8-99-515-806-nb-classifier.qza https://data.qiime2.org/2020.2/common/gg-13-8-99-515-806-nb-classifier.qza
+wget -O ./iMAP/data/references/gg-13-8-99-515-806-nb-classifier.qza https://data.qiime2.org/2019.1/common/gg-13-8-99-515-806-nb-classifier.qza
                                                                     
 if [ "$?" != "0" ]; then
     echo "[Error] Sorry, download failed, exiting...,!" 1>&2
