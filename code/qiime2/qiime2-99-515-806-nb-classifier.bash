922df418d857007305d32fc87010683f79ccdb94
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2020-07-03 13:49:47 -0500

    Update qiime2-99-515-806-nb-classifier.bash

--- code/qiime2/qiime2-99-515-806-nb-classifier.bash
+++ code/qiime2/qiime2-99-515-806-nb-classifier.bash
@@ -1,11 +1,11 @@
 
-# Greengenes classifier
+# 2019
 # wget -O ./iMAP/data/references/gg-13-8-99-515-806-nb-classifier.qza https://data.qiime2.org/2019.1/common/gg-13-8-99-515-806-nb-classifier.qza
-wget -O ./iMAP/data/references/gg-13-8-99-515-806-nb-classifier.qza https://data.qiime2.org/2020.2/common/gg-13-8-99-515-806-nb-classifier.qza
+# wget -O ./iMAP/data/references/gg-13-8-99-515-806-nb-classifier.qza https://data.qiime2.org/2020.2/common/gg-13-8-99-515-806-nb-classifier.qza
                                                           
 
-# SILVA classifier
-# wget -O ./iMAP/data/references/silva-132-99-515-806-nb-classifier.qza https://data.qiime2.org/2019.1/common/silva-132-99-515-806-nb-classifier.qza
+# 2020
+wget -O ./iMAP/data/references/silva-132-99-515-806-nb-classifier.qza https://data.qiime2.org/2019.1/common/silva-132-99-515-806-nb-classifier.qza
 # wget -O ./iMAP/data/references/silva-132-99-515-806-nb-classifier.qza https://data.qiime2.org/2020.2/common/silva-132-99-515-806-nb-classifier.qza
                                                                     
 if [ "$?" != "0" ]; then
