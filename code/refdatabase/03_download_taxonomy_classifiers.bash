b9fdbe1b07c61b4e44c79f7b41bd8d70fbe9a936
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2020-07-04 23:42:26 -0500

    Update 03_download_taxonomy_classifiers.bash

--- code/refdatabase/03_download_taxonomy_classifiers.bash
+++ code/refdatabase/03_download_taxonomy_classifiers.bash
@@ -9,7 +9,7 @@
 ## Make a taxonomy references fasta file from silva.seed.align . This will output silva.seed.fasta which automatically is placed in the data/references
 # * Clean up the directories to remove the extra files
 
-mothur "#degap.seqs(fasta=./data/references/silva.seed.align)"
+mothur "#degap.seqs(fasta=iMAP/data/references/silva.seed.align)"
 # ```
 if [ "$?" != "0" ]; then
     echo "[Error] Sorry, something may be wrong with the degap-seq, exiting...!" 1>&2
@@ -21,7 +21,7 @@ fi
 # Get HMP_MOCK.fasta - an unaligned fasta sequence file that contains sequences in the mock community
 
 wget --no-check-certificate https://www.mothur.org/MiSeqDevelopmentData/HMP_MOCK.fasta
-mv HMP_MOCK.fasta data/references
+mv HMP_MOCK.fasta iMAP/data/references
 
 # #```{}
 # rm mothur.*.logfile
