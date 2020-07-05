0a30ffeea737c1c8b11d23c077f2d9be4b770d54
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2020-07-04 22:39:20 -0500

    Update 02_download_ref_alignments.bash

--- code/refdatabase/02_download_ref_alignments.bash
+++ code/refdatabase/02_download_ref_alignments.bash
@@ -13,33 +13,15 @@ mkdir data/references
 # * Pull out the bacteria-specific sequences and
 # * Clean up the directories to remove the extra files
 
-## SILVA nr alignment
-# ```{}
-# wget --no-check-certificate https://www.mothur.org/w/images/3/32/Silva.nr_v132.tgz
-# tar xvzf Silva.nr_v132.tgz silva.nr_v132.align silva.nr_v132.tax
-# code/mothur/mothur "#get.lineage(fasta=silva.nr_v132.align, taxonomy=silva.nr_v132.tax, taxon=Bacteria);degap.seqs(fasta=silva.nr_v132.pick.align, processors=8)"
-# mv silva.nr_v132.pick.align data/references/silva.nr.align
-# mv silva.nr_v132.pick.tax data/references/silva.nr.tax
-# rm S*.tgz
-# rm silv*
-
-
-# # !For V4 datasets only
-# Generate a customized version of the SILVA reference database that targets the V4 region
-# code/mothur/mothur "#pcr.seqs(fasta=data/references/silva.nr.align, start=11894, end=25319, keepdots=F, processors=8)"
-# mv data/references/silva.nr.pcr.align data/references/silva.nr.v4.align
-
-# <br>
-
 ## SILVA seed alignment
 # ```{}
-wget --no-check-certificate https://www.mothur.org/w/images/7/71/Silva.seed_v132.tgz
-tar xvzf Silva.seed_v132.tgz silva.seed_v132.align silva.seed_v132.tax
-mothur "#get.lineage(fasta=silva.seed_v132.align, taxonomy=silva.seed_v132.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v132.pick.align, processors=8)"
-mv silva.seed_v132.pick.align data/references/silva.seed.align
-mv silva.seed_v132.pick.tax data/references/silva.seed.tax
-rm S*.tgz
-rm silv*
+wget --no-check-certificate https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.seed_v138.tgz
+tar xvzf silva.seed_v138.tgz silva.seed_v138.align silva.seed_v138.tax
+mothur "#get.lineage(fasta=silva.seed_v138.align, taxonomy=silva.seed_v138.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v138.pick.align, processors=8)"
+mv silva.seed_v138.pick.align data/references/silva.seed.align
+mv silva.seed_v138.pick.tax data/references/silva.seed.tax
+rm *ilva*
+# rm silv*
 
 if [ "$?" != "0" ]; then
     echo "[Error] Sorry, missing required reference alignment, exiting...!" 1>&2
