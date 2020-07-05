e7b525971436d60ce4b43dc702b68388930c6aa9
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2020-07-04 22:59:48 -0500

    Update 02_readPreprocess_driver.bash

--- code/02_readPreprocess_driver.bash
+++ code/02_readPreprocess_driver.bash
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-
-## Install bbmap tools (didn't work in docker image)
-bash ./code/requirements/iMAP_additional_software_driver.bash
-
-if [ "$?" != "0" ]; then
-    echo "[Error] Sorry, the bbmap tool is required, please make sure that it is installed properly. !" 1>&2
-    exit 1
-fi
-
-## Download reference databases
-# bash ./code/refdatabase/iMAP_download_driver.bash
-
 ## Read Preprocessing
 bash ./code/preprocessing/iMAP_preprocessing_driver.bash
 
