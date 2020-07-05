9d026d186b802ded23f92d8cc15df501d99e4938
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2020-07-04 22:29:13 -0500

    Update iMAP_download_driver.bash

--- code/refdatabase/iMAP_download_driver.bash
+++ code/refdatabase/iMAP_download_driver.bash
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 # bash ./code/refdatabase/01_download_refDBs.bash #iMAP 1.0
-bash ./code/refdatabase/02_download_ref_alignments.bash
-bash ./code/refdatabase/03_download_taxonomy_classifiers.bash
+bash iMAP/code/refdatabase/02_download_ref_alignments.bash
+if [ "$?" != "0" ]; then
+    echo "[Error] Sorry, reference alignments not found, exiting...!" 1>&2
+    exit 1
+fi
 
+bash iMAP/code/refdatabase/03_download_taxonomy_classifiers.bash
 
-
+if [ "$?" != "0" ]; then
+    echo "[Error] Sorry, required classifiers not found, exiting...!" 1>&2
+    exit 1
+fi
