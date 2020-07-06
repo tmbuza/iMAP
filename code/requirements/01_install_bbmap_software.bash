0b59cd5845d3e27b5ab1db3b3460ca1639b64d8b
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2020-07-06 00:22:24 -0500

    Update 01_install_bbmap_software.bash

--- code/requirements/01_install_bbmap_software.bash
+++ code/requirements/01_install_bbmap_software.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-wget --no-check-certificate https://sourceforge.net/projects/bbmap/files/BBMap_38.86.tar.gz
-tar -xvzf BBMap_38.86.tar.gz
-mv -v bbmap /imap/code/
-rm *.tar.gz
+# wget --no-check-certificate https://sourceforge.net/projects/bbmap/files/BBMap_38.86.tar.gz
+# tar -xvzf BBMap_38.86.tar.gz
+# mv -v bbmap IMAP/code/
+# rm *.tar.gz
 
-# unzip -o iMAP/resources/bbmap.zip -d iMAP/code
+unzip -o iMAP/resources/bbmap.zip -d iMAP/code
 
 if [ "$?" != "0" ]; then
     echo "[Error] Sorry, the bbmap tool is required, please make sure that it is installed properly. !" 1>&2
