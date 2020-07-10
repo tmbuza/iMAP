7a45a1864fd3ffb8129a28d148a3584ff7c18f3b
Teresia Mrema Buza <18469959+tmbuza@users.noreply.github.com>
2020-07-10 18:05:03 -0500

    Update and rename 05_get_qced_reads.bash to 05_get_qcphix_reads.bash

---
+++ code/preprocessing/05_get_qcphix_reads.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# cd ./data/raw/qctrim25
+# for i in `ls -1 *R1_001.fastq.gz | sed 's/R1_001.fastq.gz//'`
+# do
+# /imap/code/bbmap/bbduk.sh -Xmx4g in1=$i\R1_001.fastq.gz in2=$i\R2_001.fastq.gz out1=/imap/data/raw/qcphix/$i\R1_001.nophix.fastq.gz out2=/imap/data/raw/qcphix/$i\R2_001.nophix.fastq.gz outm1=/imap/data/raw/qcphix/matchedphix/$i\R1_001.matchedphix.fastq.gz outm2=/imap/data/raw/qcphix/matchedphix/$i\R2_001.matchedphix.fastq.gz ref=/imap/code/bbmap/resources/phix174_ill.ref.fa.gz k=31 hdist=1 overwrite=True
+# done
+# cd ../../../
+# fastqc /imap/data/raw/qcphix/*nophix.fastq.gz -o ./results/fastqc/qcphix/
+
+
+
+
+cd ./data/raw/qctrim25
+for i in `ls -1 *R1_001.fastq.gz | sed 's/R1_001.fastq.gz//'`
+do
+/imap/code/bbmap/bbduk.sh -Xmx4g in1=$i\R1_001.fastq.gz in2=$i\R2_001.fastq.gz out1=/imap/data/raw/qcphix/$i\R1_001.fastq.gz out2=/imap/data/raw/qcphix/$i\R2_001.fastq.gz outm1=/imap/data/raw/qcphix/matchedphix/$i\R1_001.matchedphix.fastq.gz outm2=/imap/data/raw/qcphix/matchedphix/$i\R2_001.matchedphix.fastq.gz ref=/imap/code/bbmap/resources/phix174_ill.ref.fa.gz k=31 hdist=1 overwrite=True
+done
+cd ../../../
+fastqc /imap/data/raw/qcphix/*fastq.gz -o ./results/fastqc/qcphix/
+
+cp /imap/data/raw/qced.files /imap/data/raw/qctrim25/qced.files
